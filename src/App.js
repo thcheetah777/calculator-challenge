1ec0d3a319e5f6ce6ee88ab9a84808171df41eb6
thcheetah777 <thcheetah777@gmail.com>
2023-07-01 22:25:18 +0200

    Calculator app in 1 hour

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+const typableRegex = /(^[+-/*()0-9]|Backspace|Enter|Escape|c)/g;
 
 function App() {
+  const [input, setInput] = useState("");
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const raiseError = () => {
+      setInput("NaN");
+      setError(true);
+    }
+
+    const calculate = () => {
+      try {
+        const result = eval(input);
+        if (result !== input) {
+          if (result === undefined) {
+            raiseError();
+          } else {
+            setInput(String(result));
+          }
+        }
+      } catch {
+        raiseError();
+      }
+    }
+  
+    const keyTyped = (e) => {
+      if (e.key.match(typableRegex) === null) return;
+  
+      if (e.key === "Enter") {
+        calculate();
+      } else if (e.key === "Backspace") {
+        setInput(prev => prev.slice(0, -1));
+      } else if (e.key === "Escape" || e.key === "c") {
+        setInput("");
+      } else {
+        if (error) {
+          setError(false);
+          setInput("");
+        }
+
+        setInput(prev => prev + e.key);
+      }
+  
+      e.preventDefault();
+    }
+
+    document.addEventListener("keydown", keyTyped);
+
+    return () => document.removeEventListener("keydown", keyTyped);
+  }, [input, error]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="flex justify-center items-center h-screen">
+      <div className="ball absolute -z-10" />
+      <p className={(input ? "p-5" : "border border-white") + " text-center mx-10 flex-grow rounded-full glow outline-none text-white text-3xl overflow-hidden duration-300"}>{input}</p>
+    </main>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "background": "#111111",
+        "light": "#333333"
+      }
+    },
+  },
+  plugins: [],
+}
+
